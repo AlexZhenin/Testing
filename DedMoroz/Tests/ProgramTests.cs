49ba539807479dfcd8779f3e60e828d723a11a4a
Alexey <131856761+AlexZhenin@users.noreply.github.com>
2024-12-08 11:39:16 +0500

    Create ProgramTests.cs

---
+++ DedMoroz/Tests/ProgramTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace DedMoroz.Tests
+{
+    public class ProgramTests
+    {
+        [Theory]
+        [InlineData("10 25 15 40", 3)]  // Пример входных данных и ожидаемого результата
+        [InlineData("5 10 20 30", 3)]
+        [InlineData("7 14 21 35", 2)]
+        [InlineData("1 2 3 4", 4)]
+        [InlineData("10 20 30 60", 5)]
+        public void CalculateSum_ValidInput_ReturnsExpectedSum(string input, int expected)
+        {
+            int result = Program.CalculateSum(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
