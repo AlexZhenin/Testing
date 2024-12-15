1abc949d83594eed1d4c6e85d90ff5d788ed84cb
Alexey <131856761+AlexZhenin@users.noreply.github.com>
2024-12-15 11:04:15 +0500

    Update ProgramTests.cs

--- DedMoroz/Tests/ProgramTests.cs
+++ DedMoroz/Tests/ProgramTests.cs
@@ -8,7 +8,7 @@ namespace DedMoroz.Tests
         [Theory]
         [InlineData("10 25 15 40", 3)]  // Пример входных данных и ожидаемого результата
         [InlineData("5 10 20 30", 6)]
-        [InlineData("7 14 21 35", 5)]
+        [InlineData("7 14 21 35", 4)]
         [InlineData("1 2 3 4", 4)]
         [InlineData("10 20 30 60", 7)]
         public void CalculateSum_ValidInput_ReturnsExpectedSum(string input, int expected)
